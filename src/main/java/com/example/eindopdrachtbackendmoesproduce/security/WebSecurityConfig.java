68e4900c021dfc7eab971ab094943a6f2620f7c3
MisterVerhoef <frankverhoef@outlook.com>
2022-09-17 18:02:24 +0200

    permitall antmatchers toegevoegd om advert database zonder jwt te benaderen

--- src/main/java/com/example/eindopdrachtbackendmoesproduce/security/WebSecurityConfig.java
+++ src/main/java/com/example/eindopdrachtbackendmoesproduce/security/WebSecurityConfig.java
@@ -84,7 +84,8 @@ public class WebSecurityConfig { // extends WebSecurityConfigurerAdapter {
                 .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .authorizeRequests().antMatchers("/api/auth/**").permitAll()
-                .antMatchers("/api/test/**").permitAll()
+                .antMatchers("/api/test/**").permitAll() // toegevoegd om voor testen
+                .antMatchers("/api/adverts/**").permitAll() // toegevoegd voor testen
                 .anyRequest().authenticated();
 
         http.authenticationProvider(authenticationProvider());
